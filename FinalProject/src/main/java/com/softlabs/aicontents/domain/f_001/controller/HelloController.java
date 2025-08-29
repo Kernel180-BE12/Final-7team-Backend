ab0b1fe01187a3f1a81f587866ddb88f2e570729
JungSoonIn <tnsdls1966@naver.com>
2025-08-29 12:47:21 +0900

    feat: Swagger 테스트용 API 추가

--- FinalProject/src/main/java/com/softlabs/aicontents/domain/f_001/controller/HelloController.java
+++ FinalProject/src/main/java/com/softlabs/aicontents/domain/f_001/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.softlabs.aicontents.domain.f_001.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,13 @@ public class HelloController {
         public String hello() {
             return "안녕하세요! 프로젝트가 성공적으로 시작되었습니다!";
         }
+
+    @GetMapping("/swagger-test")
+    @Operation(summary = "Swagger 테스트 API", description = "Swagger UI에서 동작을 확인하기 위한 테스트용 API입니다.")
+    public String swaggerTest() {
+        return "Swagger 연동이 정상적으로 작동 중입니다!";
+    }
+
     }
 
 
